2e3337b57b7c67d13f0ea97a77e026b940ed8ac4
Vitor <vitormoroli@gmail.com>
2023-03-10 18:43:04 -0300

    usando JQuery

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-const inputRange = document.querySelector('.gorjeta')
-const btnCalcular = document.querySelector('.calcular')
-
 const atualizarValorRange = ()=>{
-    const valorRange = document.querySelector('.valor-range')
-    valorRange.innerText = inputRange.value
+    $('.valor-range').text($('.gorjeta').val())
 }
 
 const pegaValorInputText = ()=>{
-    const inputText = document.querySelector('.conta')
-    return Number(inputText.value)
+    return Number($('.conta').val())
 }
 
 const calcularGorjeta = (total)=>{
-    const porcentagem = Number(inputRange.value)
+    const porcentagem = Number($('.gorjeta').val())
     return total * (porcentagem/100)
 }
 
 const mostrarResultados = (gorjeta,total)=>{
-    const valorGorjeta = document.querySelector('.valor-gorjeta')
-    const valorConta = document.querySelector('.valor-conta')
-    valorGorjeta.innerText = gorjeta.toFixed(2)
-    valorConta.innerHTML = total.toFixed(2)
+    $('.valor-gorjeta').text(gorjeta.toFixed(2))
+    $('.valor-conta').text(total.toFixed(2))
 }
 
 const calcular = ()=>{
@@ -30,5 +23,5 @@ const calcular = ()=>{
     mostrarResultados(gorjeta,valorTotal)
 }
 
-inputRange.addEventListener('input',atualizarValorRange)
-btnCalcular.addEventListener('click',calcular)
+$('.gorjeta').on('input',atualizarValorRange)
+$('.calcular').click(calcular)
